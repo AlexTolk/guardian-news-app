3527f094cd317755da1dd67fe883c2e67fadd6dc
Alexander Tolkachev <alexandr.tolkachyov@gmail.com>
2024-10-07 14:22:27 +0800

    styled the app

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
     try {
       const response = await fetch(url);
       const data = await response.json();
+      console.log(data.response.results)
       setNews(data.response.results);
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -32,37 +33,41 @@ function App() {
 
   return (
     <div className="App">
-      <h1>The Guardian News Search</h1>
-      <form onSubmit={handleSearch}>
-        <label>Enter a topic:
-          <input
-            type="text"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            placeholder="e.g., sports, politics, economics"
-          />
-        </label>
-        <button type="submit">Search</button>
-      </form>
-      <ul>
-      {news.map((article) => (
-        <li key={article.id}>
-          <a href={article.webUrl} target="_blank" rel="noopener noreferrer">
-            {article.webTitle}
-          </a>
-        </li>
-      ))}
-    </ul>
-    {news.length > 0 && (
-      <div>
-        <button onClick={() => setPage(page > 1 ? page - 1 : 1)}>
-          Previous Page
-        </button>
-        <button onClick={() => setPage(page + 1)}>
-          Next Page
-        </button>
+      <div className='search-box'>
+        <h1>The Guardian News Search</h1>
+        <h3>Search by topic:</h3>
+        <form onSubmit={handleSearch}>
+            <input
+              type="text"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder="e.g., sports, politics, economy"
+            />
+          <button type="submit">search</button>
+        </form>
       </div>
-    )}
+      <div className='articles-container'>
+        <ul>
+        {news.map((article) => (
+          <li key={article.id}>
+            <a href={article.webUrl} target="_blank" rel="noopener noreferrer">
+              {article.webTitle}
+            </a>
+          </li>
+        ))}
+      </ul>
+      </div>
+      {news.length > 0 && (
+        <div className='pagination-container'>
+          <button disabled={page === 1 ? true : false } onClick={() => setPage(page > 1 ? page - 1 : 1)}>
+          ← previous page
+          </button>
+          <p>page: {page}</p>
+          <button onClick={() => setPage(page + 1)}>
+            next page →
+          </button>
+        </div>
+      )}
     </div>
   );
 }
